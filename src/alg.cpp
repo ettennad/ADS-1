7fd09a9e3eb1ed0e26c7a16368d9b6f70a54aa5b
ettennad <a.derlukova@mail.ru>
2025-04-06 02:58:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,50 +4,49 @@
 
 bool checkPrime(uint64_t value) {
     if (value < 2) return false;
-    if (value == 2) return true; 
-    if (value % 2 == 0) return false;
-    for (uint64_t i = 3; i * i <= value; i += 2) {
-        if (value % i == 0) return false;
+    if (value < 4) return true;
+    if (value % 2 == 0 || value % 3 == 0) return false;
+    for (uint64_t i = 5; i * i <= value; i += 6) {
+        if (value % i == 0 || value % (i + 2) == 0) return false;
     }
     return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    if (n < 1) return 0;
-    uint64_t count = 0;
-    uint64_t num = 1;
+    if (n == 0) return 0;
+    uint64_t count = 0, number = 1;
     while (count < n) {
-        ++num;
-        if (checkPrime(num)) {
-            ++count;
-        }
+        number++;
+        if (checkPrime(number)) count++;
     }
-    return num;
+    return number;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    uint64_t next = value + 1;
-    while (!checkPrime(next)) {
-        ++next;
+    uint64_t candidate = value + 1;
+    while (!checkPrime(candidate)) {
+        candidate++;
     }
-    return next;
+    return candidate;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; ++i) {
-        if (checkPrime(i)) {
-            sum += i;
-        }
+        if (checkPrime(i)) sum += i;
     }
     return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-    uint64_t count = 0;
-    for (uint64_t i = lbound; i < hbound - 2; ++i) {
-        if (checkPrime(i) && checkPrime(i + 2)) {
-            ++count;
+    if (hbound < 3) return 0;
+    uint64_t count = 0, prevPrime = 0;
+    for (uint64_t i = lbound; i < hbound; ++i) {
+        if (checkPrime(i)) {
+            if (prevPrime != 0 && i - prevPrime == 2) {
+                count++;
+            }
+            prevPrime = i;
         }
     }
     return count;
