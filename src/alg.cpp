ecadec0a3c9265bdb9682556d706fb23e69ab286
ettennad <a.derlukova@mail.ru>
2025-04-06 18:50:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2025 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 
@@ -6,14 +6,10 @@ bool checkPrime(uint64_t value) {
   if (value < 2) {
     return false;
   }
-  if (value < 4) {
-    return true;
-  }
-  if (value % 2 == 0 || value % 3 == 0) {
-    return false;
-  }
-  for (uint64_t i = 5; i * i <= value; i += 6) {
-    if (value % i == 0 || value % (i + 2) == 0) return false;
+  for (uint64_t i = 2; i * i <= value; ++i) {
+    if (value % i == 0) {
+      return false;
+    }
   }
   return true;
 }
@@ -51,9 +47,7 @@ uint64_t sumPrime(uint64_t hbound) {
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  if (hbound < 3) {
-    return 0;
-  }
+  if (hbound < 3) return 0;
   uint64_t count = 0, prevPrime = 0;
   for (uint64_t i = lbound; i < hbound; ++i) {
     if (checkPrime(i)) {
