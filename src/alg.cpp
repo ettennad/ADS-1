de0dc3d3b1ef41b84c92b37169ce6077c36965d3
ettennad <a.derlukova@mail.ru>
2025-04-27 14:17:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,79 +4,79 @@
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
-    if (value < 2) {
-        return false;
-    } else if (value == 2) {
-        return true;
-    } else if (value % 2 == 0) {
-        return false;
-    }
+  if (value < 2) {
+      return false;
+  } else if (value == 2) {
+      return true;
+  } else if (value % 2 == 0) {
+      return false;
+  }
 
-    for (uint64_t i = 3; i * i <= value; i += 2) {
-        if (value % i == 0) {
-            return false;
-        }
-    }
-    return true;
+  for (uint64_t i = 3; i * i <= value; i += 2) {
+      if (value % i == 0) {
+          return false;
+      }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    if (n == 0) {
-        return 0;
-    }
+  if (n == 0) {
+      return 0;
+  }
 
-    uint64_t count = 0;
-    uint64_t number = 1;
+  uint64_t count = 0;
+  uint64_t number = 1;
 
-    while (count < n) {
-        number++;
-        if (checkPrime(number)) {
-            count++;
-        }
-    }
-    return number;
+   while (count < n) {
+      number++;
+      if (checkPrime(number)) {
+          count++;
+      }
+  }
+  return number;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    uint64_t candidate = value + 1;
-    while (!checkPrime(candidate)) {
-        candidate++;
-    }
-    return candidate;
+  uint64_t candidate = value + 1;
+  while (!checkPrime(candidate)) {
+      candidate++;
+  }
+  return candidate;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum = 0;
-    for (uint64_t i = 2; i < hbound; ++i) {
-        if (checkPrime(i)) {
-            sum += i;
-        }
-    }
-    return sum;
+  uint64_t sum = 0;
+  for (uint64_t i = 2; i < hbound; ++i) {
+      if (checkPrime(i)) {
+          sum += i;
+      }
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-    uint64_t count = 0;
-    uint64_t prevPrime = 0;
+  uint64_t count = 0;
+  uint64_t prevPrime = 0;
 
-    for (uint64_t i = lbound; i < hbound; ++i) {
-        if (checkPrime(i)) {
-            prevPrime = i;
-            break;
-        }
-    }
+  for (uint64_t i = lbound; i < hbound; ++i) {
+      if (checkPrime(i)) {
+          prevPrime = i;
+          break;
+      }
+  }
 
-    if (prevPrime == 0) {
-        return 0;
-    }
+  if (prevPrime == 0) {
+      return 0;
+  }
 
-    for (uint64_t i = prevPrime + 1; i < hbound; ++i) {
-        if (checkPrime(i)) {
-            if (i - prevPrime == 2) {
-                count++;
-            }
-            prevPrime = i;
-        }
-    }
-    return count;
+  for (uint64_t i = prevPrime + 1; i < hbound; ++i) {
+      if (checkPrime(i)) {
+          if (i - prevPrime == 2) {
+              count++;
+          }
+          prevPrime = i;
+      }
+  }
+  return count;
 }
