3c000f03717226d3f0b627e39fe2edae92dd38ad
ettennad <a.derlukova@mail.ru>
2025-04-27 14:21:30 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,18 @@
 // Copyright 2022 NNTU-CS
+#include <iostream> 
 #include "alg.h"
 
 int main() {
-    uint64_t value = 29;
-    std::cout << checkPrime(value) << std::endl;
-
-    uint64_t n = 10;
-    std::cout << nPrime(n) << std::endl;
-
-    std::cout << nextPrime(value) << std::endl;
-
-    uint64_t hbound = 50;
-    std::cout << sumPrime(hbound) << std::endl;
-
-    uint64_t lbound = 2;
-    std::cout << twinPrimes(lbound, hbound) << std::endl;
-
-    return 0;
+  uint64_t value = 29;
+  uint64_t n = 10;
+  uint64_t hbound = 50;
+  uint64_t lbound = 2;
+  
+  std::cout << checkPrime(value) << std::endl;
+  std::cout << nPrime(n) << std::endl;
+  std::cout << nextPrime(value) << std::endl;
+  std::cout << sumPrime(hbound) << std::endl;
+  std::cout << twinPrimes(lbound, hbound) << std::endl;
+  
+  return 0;
 }
