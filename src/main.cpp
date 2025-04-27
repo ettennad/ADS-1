17315513d0bb781a9d8f33b9683193c9774bc510
ettennad <a.derlukova@mail.ru>
2025-04-27 14:45:05 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,10 @@ int main() {
   uint64_t n = 10;
   uint64_t hbound = 50;
   uint64_t lbound = 2;
-  
   std::cout << checkPrime(value) << std::endl;
   std::cout << nPrime(n) << std::endl;
   std::cout << nextPrime(value) << std::endl;
   std::cout << sumPrime(hbound) << std::endl;
   std::cout << twinPrimes(lbound, hbound) << std::endl;
-  
   return 0;
 }
