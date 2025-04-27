f24ad1f055886f187d9a77d19792d7af95d4bd21
ettennad <a.derlukova@mail.ru>
2025-04-27 14:44:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,21 +1,14 @@
 // Copyright 2025 NNTU-CS
 #include <cstdint>
-#include <iosream>
+#include <iostream>
 #include <cmath>
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
   if (value < 2) {
     return false;
-  } eles if (value == 2) {
-    return true;
-  } else if (value % 2 == 2) {
-    return true;
-  } else if (value % 2 == 0) {
-    return false;
   }
-
-  for (uint64_t i = 3; i * i <= value; i += 2) {
+  for (uint64_t i = 2; i * i <= value; ++i) {
     if (value % i == 0) {
       return false;
     }
@@ -27,7 +20,6 @@ uint64_t nPrime(uint64_t n) {
   if (n == 0) {
     return 0;
   }
-  
   uint64_t count = 0;
   uint64_t number = 1;
   while (count < n) {
@@ -60,18 +52,15 @@ uint64_t sumPrime(uint64_t hbound) {
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t count = 0;
   uint64_t prevPrime = 0;
-  
   for (uint64_t i = lbound; i < hbound; ++i) {
     if (checkPrime(i)) {
       prevPrime = i;
       break;
     }
   }
-  
   if (prevPrime == 0) {
     return 0;
   }
-  
   for (uint64_t i = prevPrime + 1; i < hbound; ++i) {
     if (checkPrime(i)) {
       if (i - prevPrime == 2) {
