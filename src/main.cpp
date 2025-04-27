04834562b21a99a4df0486505a1e5e44606bf1ad
ettennad <a.derlukova@mail.ru>
2025-04-27 14:13:02 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,19 @@
 #include "alg.h"
 
 int main() {
+    uint64_t value = 29;
+    std::cout << checkPrime(value) << std::endl;
+
+    uint64_t n = 10;
+    std::cout << nPrime(n) << std::endl;
+
+    std::cout << nextPrime(value) << std::endl;
+
+    uint64_t hbound = 50;
+    std::cout << sumPrime(hbound) << std::endl;
+
+    uint64_t lbound = 2;
+    std::cout << twinPrimes(lbound, hbound) << std::endl;
+
     return 0;
 }
